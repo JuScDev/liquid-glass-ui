fbc0d724844f3940f89b88d68a2b9927b97ff36c
Julian Scheiner <contact@julian-scheiner.de>
2025-10-28 18:40:00 +0000

    feat: add LiquidMusicWidget component and integrate into app

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { LiquidButton } from './liquid-button/liquid-button';
 import { LiquidCard } from './liquid-card/liquid-card';
+import { LiquidMusicWidget } from './liquid-music-widget/liquid-music-widget';
 
 @NgModule({
-  declarations: [App, LiquidButton, LiquidCard],
+  declarations: [App, LiquidButton, LiquidCard, LiquidMusicWidget],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
---
+++ src/app/liquid-music-widget/liquid-music-widget.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liquid-music-widget',
+  standalone: false,
+  templateUrl: './liquid-music-widget.html',
+  styleUrl: './liquid-music-widget.scss',
+})
+export class LiquidMusicWidget {
+
+}
